eeb94bae56766dfa1bf2c22c2825c00cdce83303
ReoKM <reomessi658@gmail.com>
2024-08-02 22:13:03 +0900

    create_local_strage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,38 @@
 <template>
   <div>
     <h1>しおり</h1>
-    <div><input type="checkbox">ペンライト</div>
-    <div><input type="checkbox">ペンライト</div>
-    <div><input type="checkbox">ペンライト</div>
-    <div><input type="checkbox">ペンライト</div>
+    <div v-for="(item, index) in items" :key="index">
+      <input type="checkbox" v-model="item.checked" @change="saveItems">
+      {{ item.text }}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { text: 'ペンライト', checked: false },
+        { text: 'うちわ', checked: false },
+        { text: 'タオル', checked: false },
+        { text: '水', checked: false }
+      ]
+    }
+  },
+  methods: {
+    saveItems() {
+      localStorage.setItem('shiorItems', JSON.stringify(this.items))
+    },
+    loadItems() {
+      const savedItems = localStorage.getItem('shiorItems')
+      if (savedItems) {
+        this.items = JSON.parse(savedItems)
+      }
+    }
+  },
+  mounted() {
+    this.loadItems()
+  }
+}
+</script>
