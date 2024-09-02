f8c54d71ba237b2340d26aa312ba48c5f4acbc13
ReoKM <reomessi658@gmail.com>
2024-09-02 12:24:37 +0900

    create pr

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -3,14 +3,14 @@
     <div class="modal-content">
       <h2>しおりの使い方</h2>
       <p>このしおりは<strong>同じ端末・ブラウザ</strong>で、チェックリストの内容が保存されます！</p>
-      <p>しおりを利用される際は、ホーム画面にウェブサイトを追加することを推奨します。</p>
+      <p>しおりを利用される際は、<strong>ホーム画面にウェブサイトを追加</strong>することを推奨します。</p>
       <h3>iPhoneをお使いの方</h3>
-      <p>1. ウェブブラウザの画面内にある共有ボタンをクリック</p>
+      <p>1. ウェブブラウザの画面内にある<strong><img src="../assets/share.svg" class="share-icon" />共有ボタン</strong>をクリック</p>
       <p>2. 表示された画面から「ホーム画面に追加」をタップして完了</p>
       <h3>Androidをお使いの方</h3>
-      <p>1. ウェブブラウザの画面内にある「⋮」をクリック</p>
+      <p>1. ウェブブラウザの画面内にある「<strong>⋮</strong>」をクリック</p>
       <p>2. 表示された画面から「ホーム画面に追加」をタップして完了</p>
-      <button @click="closeModal">閉じる</button>
+      <button  class="button" @click="closeModal">閉じる</button>
     </div>
   </div>
 </template>
@@ -38,6 +38,27 @@ export default {
 </script>
 
 <style>
+strong {
+  color: var(--color-check);
+}
+
+h2 {
+  margin: 0;
+}
+h3 {
+  margin: 24px 0 8px 0;
+}
+
+.share-icon {
+  height: 16px;
+  padding: 0 2px;
+  fill: var(--color-check);
+}
+
+p {
+  margin: 8px 0;
+}
+
 .modal-overlay {
   position: fixed;
   top: 0;
@@ -53,9 +74,9 @@ export default {
 
 .modal-content {
   background: white;
-  padding: 20px;
+  padding: 24px;
   border-radius: 8px;
-  width: 90%;
+  width: 80%;
   max-width: 400px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
   text-align: center;
