d99cd9ec10261149d85878f7e8e0cf0168f2abad
ReoKM <reomessi658@gmail.com>
2024-09-07 02:06:27 +0900

    note update

---
+++ src/views/noteshow/note13.js
@@ -0,0 +1,8 @@
+export const note13 = {
+  id: 13,
+  title: 'タイムテーブル公開！',
+  content: `
+  <p>ひなたフェスに関するタイムテーブルが公開されました！</p>
+  <a href="https://cdn.hinatazaka46.com/images/14/f98/28409675c970e62f6a94cbb7ab3c0.jpg?_gl=1*13aqfu6*_ga*OTI3OTE4MDg1LjE3MTU1MjM0MjA.*_ga_RPGRKJP9NK*MTcyNTYyNDc3OC40MS4xLjE3MjU2MjUzNjQuMjEuMC4w" target="_blank">ひなたフェスタイムテーブル</a><br>
+  `
+};
